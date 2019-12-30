d1a3b24791cc6400973b2facc69a996c04cb6262
RD <rory.c.dickinson@gmail.com>
2019-12-29 19:28:37 -0600

    show win when ready

--- app/main.js
+++ app/main.js
@@ -6,9 +6,16 @@ let mainWindow = null;
 app.on('ready', () => {
 
     mainWindow = new BrowserWindow({
+        show: false
     });
 
     mainWindow.loadFile('app/index.html');
+
+    // add a single event listener to the window's ready-to-show event
+    mainWindow.once('ready-to-show', () => {
+        // show the window when the DOM is ready
+        mainWindow.show();
+    });
 
     mainWindow.on('closed', () => {
         mainWindow = null;
