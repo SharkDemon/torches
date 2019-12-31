0fa30ceff9624c9c7db82f2526c3d0d422779eea
RD <rory.c.dickinson@gmail.com>
2019-12-30 21:03:27 -0600

    get the open dialog correct for this version of electron

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
 
 let mainWindow = null;
 
@@ -19,9 +20,28 @@ app.on('ready', () => {
         // show the window when the DOM is ready
         mainWindow.show();
         //mainWindow.webContents.openDevTools();
+        getFileFromUser();
     });
 
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
 });
+
+// triggers the operating system's Open File dialog box
+const getFileFromUser = () => {
+
+    dialog.showOpenDialog(mainWindow, {
+        properties: ['openFile'],
+        filters: [
+            { name: 'Text Files', extensions: ['txt'] },
+            { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+        ]
+    }).then( (data) => {
+        // pull the first file from the array
+        const file = data.filePaths[0];
+        // read the file, convert resulting buffer to a string
+        const content = fs.readFileSync(file).toString();
+        console.log(content);
+    } );
+};
