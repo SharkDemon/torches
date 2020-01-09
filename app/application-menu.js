ca2a7223742401d39b7731c633424b2bd230608f
RD <rory.c.dickinson@gmail.com>
2020-01-08 20:11:09 -0600

    continue menu, for macos

--- app/application-menu.js
+++ app/application-menu.js
@@ -78,6 +78,33 @@ if (process.platform === 'darwin') {
                 label: 'Services',
                 role: 'services',
                 submenu: []
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: `Hide ${name}`,
+                accelerator: 'Command+H',
+                role: 'hide',
+            },
+            {
+                label: `Hide Others`,
+                accelerator: 'Command+Alt+H',
+                role: 'hideothers',
+            },
+            {
+                label: `Show All`,
+                role: 'unhide',
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: `Quit ${name}`,
+                accelerator: 'Command+Q',
+                // there is no built-in role for quitting app; instead add a click method
+                // that fires when menu item is clicked or keyboard shortcut used
+                click() { app.quit(); }
             }
         ]
     });
